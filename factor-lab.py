64077d96326a4822a58c06701402851cbf645979
timrolsh <tim.rolsh@gmail.com>
2023-02-10 10:11:51 -0500

    do factor lab, add instructions for labs

---
+++ factor-lab.py
@@ -0,0 +1,142 @@
+import math
+small_primes: list[int] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+
+"""
+return a tuple (s, d) from n such that n - 1 = 2^s * d
+"""
+
+
+def factor_two(n: int) -> tuple[int, int]:
+    s: int = 0
+    n -= 1
+    while n % 2 == 0:
+        s += 1
+        n = n // 2
+    # at this point, d will be equal to s
+    return (s, n)
+
+
+"""
+Return if a number is prime or not using the deterministic Miller-Rabin algorithm.
+"""
+
+
+def is_prime(n: int) -> bool:
+    if n == 0 or n == 1:
+        return False
+    elif n == 2:
+        return True
+    s, d = factor_two(n)
+    for a in range(2, int(min(n-2, 2 * math.log(n) ** 2))):
+        x: int = a ** d % n
+        for _ in range(s):
+            y: int = x ** 2 % n
+            if y == 1 and x != 1 and x != n - 1:
+                return False
+            x = y
+        if x != 1:
+            return False
+    return True
+
+
+"""
+Naive algorithm for getting prime factorization of a number, try factoring using the first 12 prime numbers.
+Returns a list. factor(60) = 2 ^ 2 * 3 ^ 1 * 5 ^ 1 Stored as a list, [2,2,3,1,5,1]
+"""
+
+
+def factor_naive(n: int) -> list[int]:
+    output: list[int] = []
+    for small_prime in small_primes:
+        count: int = 0
+        while n % small_prime == 0:
+            count += 1
+            n //= small_prime
+        if count > 0:
+            output += [small_prime, count]
+    if n != 1:
+        output += [n, 1]
+    return output
+
+
+"""
+-1 indicates a failure to find a factor
+The factor that pollard rho gives you isn't necessarily a prime factor.
+"""
+
+
+def factor_pollard_rho(n: int) -> int:
+    if n == 1:
+        return -1
+
+    def g(x):
+        return (x ** 2 + 1) % n
+    x: int = 2
+    y: int = 2
+    d: int = 1
+    while d == 1:
+        x = g(x)
+        y = g(g(y))
+        d = math.gcd(abs(x - y), n)
+    if d == n:
+        return -1
+    else:
+        return d
+
+
+"""
+Get the prime factorization of a positive integer. Returns a list.
+factor(60) = 2 ^ 2 * 3 ^ 1 * 5 ^ 1
+Stored as a list, [2,2,3,1,5,1]
+First 12 prime numbers: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37
+Start by using naive method, and if it doesn't return full prime factorization, use pollard rho to get additional factors.
+If pollard rho gives you a factor that isn't prime, get the prime factorization of that factor as well.
+"""
+
+
+def factor(n: int) -> list[int]:
+    final_dict: dict[int, int] = {}
+
+    def factor_sub(n: int) -> None:
+        # use naive method first, if the last factor is not prime, use the pollard rho method to keep factoring it
+        naive: list[int] = factor_naive(n)
+        # if naive didn't factor all the way
+        if naive[len(naive) - 1] == 1 and not is_prime(naive[len(naive) - 2]):
+            # add all factors except the last ones to the map
+            for a in range(0, len(naive) - 2, 2):
+                if naive[a] in final_dict:
+                    final_dict[naive[a]] += naive[a+1]
+                else:
+                    final_dict[naive[a]] = naive[a+1]
+            # prime factorize the last factor that naive didn't get
+            while True:
+                factor: int = factor_pollard_rho(n)
+                if factor == -1:
+                    break
+                # if factor isn't prime, get the prime factorization of it and continue
+                if not is_prime(factor):
+                    factor_sub(factor)
+                else:
+                    if factor in final_dict:
+                        final_dict[factor] += 1
+                    else:
+                        final_dict[factor] = 1
+                n //= factor
+            if n != 1:
+                if n in final_dict:
+                    final_dict[n] += 1
+                else:
+                    final_dict[n] = 1
+        else:
+            for a in range(0, len(naive), 2):
+                if naive[a] in final_dict:
+                    final_dict[naive[a]] += naive[a+1]
+                else:
+                    final_dict[naive[a]] = naive[a+1]
+    # call helper
+    factor_sub(n)
+    # convert from dictionary format to organized list format
+    final: list[int] = []
+    for a in sorted(list(final_dict.keys())):
+        final += [a, final_dict[a]]
+    return final
