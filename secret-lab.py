324023262043ac99c6a8cf321f15f566e4696b20
timrolsh <tim.rolsh@gmail.com>
2023-03-15 21:11:53 -0400

    fixed secret-lab

--- secret-lab.py
+++ secret-lab.py
@@ -1,33 +1,21 @@
 import random
-# both ways stuff
+# both ways stuff, shared by both me and john
 p: int = 808241878912418587664668869752380201227471917587049402831353579654087934716257177583988001816750842723486468130003910337833297294744103045277794137039348379685305766000049233920274403424909809118398706373176342402680527597912453031947055263965574521088574747656702199527971035049821214553730158203939
 g: int = 2
 
-# nevards public key
-a: int = 455848071374707988038292715962272655511109957743547844562644188078424249523058593328745877427412302513863218445493774359827883299407761443769628022340049190244708960432307664952518902871204598305338226446449763051563904730839041083188049120606722799230110813191873851538379238410155389668523135874520
+# john's public key, i use this to encrypt my message and send it to him, given to me by john
+a: int = 232776343254475000399703950622050363150042223625177014198929234401212675210038579218292434526526374082925071163131994952558899624844364841082089396570570225786199190021288348405073108777200323973770403536044277880631661314326452532373031693236191798967750863498867080388017998943491279903675203454640
 
-# my private key
-private_key: int = 103011687869025962938928581392454646913888320358058107007013214592943394893600843706555430866101403805500468594293363551417065855504377689726869263349230962223056262241682559158559471202130897968724088885318548014335612047339112293175474246206940925747986708262498155968303127474846337858464828170526
-# my public key
-public_key: int = 758048007388692678051507134925220147949193210592171917492886415049292890408600729304845356456379007068998106141757681688120537472649113820008264554026611845089693977721029362780526767792997143297924494233690208588085357392567928582289215093295258969125467405944520593705719666381621302537173748806787
+# my private key, i use this to decrypt what john sends to me, i made this one randomly
+private_key: int
+# my public key, i send this to john so he can encrypt something and send it back to me, i made this one randomly
+public_key: int
 
-
-plain_text: str = "The German motor company, BMW, has offered their customers a wide selection of different luxury and performance cars. From sedans, to SUVs, to coupes, and hatchbacks, BMW has many different series options, trims, and models to offer to the luxury and performance vehicle market. However, one specific car stands out from the others. The sport or performance model of their 4 series, 2 door coupe, called the M4 (the M represents the BMW M power performance line, while the 4 represents the series of the car) really shines because as a powerful performance vehicle, built to seamlessly handle at high speeds. BMW has produced two generations of the M4: one built on the F82 chassis, and one built on the G82 chassis. The focus of this paper will be on the first generation F82 model. The F82 M4 proved itself not only through statistics, numbers, and horsepower, but through its consistent, powerful performance in the real world, both on the streets and on the track."
-
-
-def mod_exp(base: int, exp: int, power: int) -> int:
-    x: int = 1
-    y: int = base
-    while exp > 0:
-        if exp % 2 != 0:
-            x = (x * y) % power
-        y = (y * y) % power
-        exp = int(exp / 2)
-
-    return x % power
+# this is the text that i will be encrypting and sending to john using the public key gave to me, only he can decrypt it
+message: str
 
 
-
+# convert a message string into a list of integers that will be small enough to fit into the prime john gave to me
 def make_message_blocks(string: str) -> list[int]:
     final_list: list[int] = []
     current_number: int = 0
@@ -47,6 +35,8 @@ def make_message_blocks(string: str) -> list[int]:
         index -= 1
     return final_list
 
+# given the message blocks, turn them back into a readable string, used by decrypt to produce a final result
+
 
 def message_blocks_to_message(message_blocks: list[int]) -> str:
     a: int = 0
@@ -64,27 +54,43 @@ def message_blocks_to_message(message_blocks: list[int]) -> str:
         a += 1
     return "".join(final_list)
 
+# encrypt a block of messages using a public key
+
 
 def encrypt(message_blocks: list[int]) -> list[tuple[int, int]]:
     final_list: list[tuple[int, int]] = []
     j: int = random.randrange(2, p - 1)
     for B in message_blocks:
-        # todo remember to swap the public key here
-        final_list.append((mod_exp(g, j, p), B * mod_exp(public_key, j, p)))
+        # if you want to encrypt something and then descrypt it uself, swap a for public_key
+        final_list.append((pow(g, j, p), (pow(a, j, p) * B) % p))
     return final_list
+
+# decrypted an encrypted block of numbers that represent messages
 
 
 def decrypt(encrypted_messages: list[tuple[int, int]]) -> str:
     decrypted_list: list[int] = []
     index: int = 0
     while index < len(encrypted_messages):
-        decrypted_list.append(mod_exp(encrypted_messages[index][1] * encrypted_messages[index][0], (p - 1 - private_key), p))
+        decrypted_list.append((encrypted_messages[index][1] * pow(
+            encrypted_messages[index][0], (p - 1 - private_key), p)) % p)
         index += 1
     return message_blocks_to_message(decrypted_list)
 
-# print(message_blocks_to_message(make_message_blocks(plain_text)))
 
-encrypted_message = encrypt(make_message_blocks(plain_text))
-decrypted_message = decrypt(encrypted_message)
+# create a private key and a public key and a message, save the output of this program as every time it runs is generates new keys
+message = "why was 6 afraid of 7? cause 7 8 0 "
+print("The plain text message is:\n" + message)
+private_key = random.randrange(2, p - 1)
+print(f"The private key is:\n{private_key}")
+public_key = pow(g, private_key, p)
+print("SEND EVERYTHING AFTER THIS LINE TO JOHN. DO NOT SEND ANYTHING ABOVE. REWRITE THE LINE BELOW IN YOUR OWN WORDS, AND REWRITE THE PUBLIC KEY LINE IN YOUR OWN WORDS AS WELL.")
+# encrypt my message using john's public key, send him the result of the encrypted stuff so that he can decrypt it
+encrypted_message: list[tuple[int, int]] = encrypt(
+    make_message_blocks(message))
+print("Here is the encrypted message. It is in the format c1, c2, where c2 is the number with raw message number packed into it. ")
+for a in encrypted_message:
+    print(f"{a[0]}, {a[1]}")
 
-print(decrypted_message)
+print(f"The public key that corresponds to the private key I have generated is:\n{public_key}")
+# when it is time to decrypt a message, load the message he sends into format used by decrypt, a list of tuple consisting of c1, c2
